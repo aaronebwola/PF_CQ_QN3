f19595200e41f5e84d1fe7d07eac84cf59937458
aaronebwola <aaronolao@gmail.com>
2024-09-04 06:49:01 +0300

    Add files via upload

---
+++ reduce.h
@@ -0,0 +1,8 @@
+// Others/reduce.h
+#ifndef REDUCE_H
+#define REDUCE_H
+
+int reduce(int &num, int &denom);
+int gcd(int a, int b);
+
+#endif
---
+++ sum_from_to.h
@@ -0,0 +1,7 @@
+// Others/sum_from_to.h
+#ifndef SUM_FROM_TO_H
+#define SUM_FROM_TO_H
+
+int sum_from_to(int first, int last);
+
+#endif
